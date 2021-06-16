f76cf398f2208b47a7abc135e91b29f9d4a9c6ba
LukeTri <54450751+LukeTri@users.noreply.github.com>
2021-06-16 12:13:02 -0400

    Add files via upload

--- HaldaneCode.py
+++ HaldaneCode.py
@@ -33,23 +33,64 @@ def Hamiltonian(N=30, v=1.0, w=1.0, show=False, u=1.0, t=1.0):
 
 out = Hamiltonian(100, 2, 5, False, 1, 10.0)
 
+#large Hamiltonian matrix for the periodic Haldane model
+
+def LargeHamiltonian(N, v, w,u,t):
+    aH = np.zeros((2 * N * N, 2 * N * N), dtype='complex')
+    for n in range(0,N):
+        for m in range(0,N):
+            #NN pairs
+            aH[2*N*n + 2*m][2*N*n + 2*m+1] = v
+            aH[2 * N * n + 2 * m + 1][2 * N * n + 2 * m] = v
+
+            aH[2*N*n + 2*m][2*N*((n-1)%N) + 2*m + 1] = w
+            aH[2 * N * ((n - 1) % N) + 2 * m + 1][2 * N * n + 2 * m] = w
+
+            aH[2 * N * n + 2 * m][2 * N * ((n - 1) % N) + 2 * ((m+1)%N) + 1] = w
+            aH[2 * N * ((n - 1) % N) + 2 * ((m + 1) % N) + 1][2 * N * n + 2 * m] = w
+
+            #NNN same row
+            aH[2*N*n + 2*m][2*N*n + 2*((m+1)%N)] = u
+            aH[2 * N * n + 2 * m +1][2 * N * n + 2 * ((m + 1) % N)+1] = u
+
+            aH[2 * N * n + 2 * m][2 * N * n + 2 * ((m - 1) % N)] = u
+            aH[2 * N * n + 2 * m + 1][2 * N * n + 2 * ((m - 1) % N) + 1] = u
+
+            #NNN above row
+            aH[2 * N * n + 2 * m][2 * N * ((n - 1) % N) + 2 * m] = u
+            aH[2 * N * n + 2 * m+1][2 * N * ((n - 1) % N) + 2 * m+1] = u
+
+            aH[2 * N * n + 2 * m][2 * N * ((n-1)%N) + 2 * ((m + 1) % N)] = u
+            aH[2 * N * n + 2 * m+1][2 * N * ((n - 1) % N) + 2 * ((m + 1) % N)+1] = u
+
+            #NNN below row
+            aH[2 * N * n + 2 * m][2 * N * ((n + 1) % N) + 2 * m] = u
+            aH[2 * N * n + 2 * m + 1][2 * N * ((n + 1) % N) + 2 * m + 1] = u
+
+            aH[2 * N * n + 2 * m][2 * N * ((n + 1) % N) + 2 * ((m - 1) % N)] = u
+            aH[2 * N * n + 2 * m + 1][2 * N * ((n + 1) % N) + 2 * ((m - 1) % N) + 1] = u
+    print(aH)
+    print(min(np.absolute(np.linalg.eig(aH)[0])))
+
+LargeHamiltonian(3,1,2,3,0)
+
 
 # v is amplitude between red and blacks, u is between same color
 #aperHamiltonian constructs the 2*n by 2*n aperiodic hamiltonian
 
-def aperHamiltonian(N, v, u):
+def aperHamiltonian(N, v, w,u,t):
     aH = np.zeros((2 * N * N, 2 * N * N), dtype='complex')
     for i in range(0, 2 * N * N):
         if i % 2 == 0:
             aH[i][i + 1] = v
             aH[i + 1][i] = v
             if i / (2 * N) != 0:
-                aH[i][i - 2 * N + 1] = v
-                aH[i - 2 * N + 1][i] = v
+                aH[i][i - 2 * N + 1] = w
+                aH[i - 2 * N + 1][i] = w
 
                 if (i - 2 * N + 3) % (2 * N) != 1:
-                    aH[i][i - 2 * N + 3] = v
-                    aH[i - 2 * N + 3][i] = v
+                    aH[i][i - 2 * N + 3] = w
+                    aH[i - 2 * N + 3][i] = w
 
             if (i + 2) % (2 * N) != 0:
                 aH[i][i + 2] = u
@@ -87,33 +128,13 @@ def aperHamiltonian(N, v, u):
 
                     aH[i + 1][i + 2 * N - 1] = u
                     aH[i + 2 * N - 1][i + 1] = u
-    return aH
+    print(aH)
+    print(min(np.absolute(np.linalg.eig(aH)[0])))
 
-
-def getVec(kx, ky, c, N, v):
-    vec = np.zeros((2 * N * N), dtype='complex')
-    vec[2 * ky * N + 2 * kx + c] = cmath.exp(1j * (ky * v)) + cmath.exp(
-        1j * (kx * math.sqrt(3) / 2 + ky * -v / 2)) + cmath.exp(1j * (kx * math.sqrt(3) / 2 + ky * -v / 2))
-    return vec
-
-
-# aperBerry method gives NxN array of the B-M hamiltonians for the aperiodic case (same form as Hamiltonian function)
-
-def aperBerry(N, v, w):
-    matr = np.zeros((N, N, 2, 2), dtype='complex')
-    aH = aperHamiltonian(N, v, w)
-    for kx in range(0, N):
-        for ky in range(0, N):
-            temp = np.zeros((2, 2), dtype='complex')
-            for A in range(0, 2):
-                for B in range(0, 2):
-                    temp[A][B] = np.dot(np.matmul(getVec(kx, ky, A, N, v), aH), getVec(kx, ky, B, N, v))
-            matr[kx][ky] = temp
-    return matr
-
+aperHamiltonian(2,1,0,0,0)
 
 def Berry(N, v, w, u, t):
-    matr = aperBerry(N, v, w)
+    matr = Hamiltonian(N, v, w,u,t)
     pos = np.zeros((N, N, 2))
 
     dat = np.zeros((N, N, 2 * N * N), dtype='complex')
@@ -122,24 +143,22 @@ def Berry(N, v, w, u, t):
         for ky in range(1, N + 1):
 
             vec = np.linalg.eig(matr[kx - 1][ky - 1])[1][:, np.argmax(np.linalg.eig(matr[kx - 1][ky - 1])[0])]
-            # vec = vec/vec[0]
+            #vec = vec/vec[0]
             # vec = np.array([1,matr[kx-1][ky-1][0][1]/cmath.polar(matr[kx-1][ky-1][0][1])[0]])
 
             for n in range(0, N):
                 for m in range(0, N):
-                    dat[kx - 1][ky - 1][(2 * n) * N + 2 * m] = cmath.exp(((2 * math.pi) / N) * (kx * n + ky * m)) * vec[
+                    dat[kx - 1][ky - 1][(2 * n) * N + 2 * m] = cmath.exp(((2j * math.pi) / N) * (kx * n + ky * m)) * vec[
                         0]
-                    dat[kx - 1][ky - 1][(2 * n) * N + 2 * m + 1] = cmath.exp(((2 * math.pi) / N) * (kx * n + ky * m)) * \
+                    dat[kx - 1][ky - 1][(2 * n) * N + 2 * m + 1] = cmath.exp(((2j * math.pi) / N) * (kx * n + ky * m)) * \
                                                                    vec[1]
             dat[kx - 1][ky - 1] = dat[kx - 1][ky - 1] / np.linalg.norm(dat[kx - 1][ky - 1])
     sumterm = 0
     for n in range(0, N):
         for m in range(0, N):
-            sumterm = sumterm - cmath.polar(np.vdot(dat[n][m], dat[(n + 1) % N][m]) * np.vdot(dat[(n + 1) % N][m],
-                                                                                              dat[(n + 1) % N][(
-                                                                                                                       m + 1) % N]) * np.vdot(
-                dat[(n + 1) % N][(m + 1) % N], dat[n][(m + 1) % N]) * np.vdot(dat[n][(m + 1) % N], dat[n][m]))[1]
+            sumterm = sumterm - cmath.polar(np.vdot(dat[n][m], dat[(n + 1) % N][m]) * np.vdot(dat[(n + 1) % N][m],dat[(n + 1) % N][(
+                m + 1) % N]) * np.vdot(dat[(n + 1) % N][(m + 1) % N], dat[n][(m + 1) % N]) * np.vdot(dat[n][(m + 1) % N], dat[n][m]))[1]
     print(sumterm / (2 * math.pi))
 
 
-Berry(4, 2, 7, 1, 0.0)
+Berry(N=2, v=2, w=7, u=1, t=0.0)
